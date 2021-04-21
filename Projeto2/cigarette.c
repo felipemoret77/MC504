c227286b70bfa4c80eb6ef7f017d246ba18a1124
Felipe Moret <felipebuenomoret77@gmail.com>
2021-04-21 02:24:14 +0200

    Log working

--- Projeto2/cigarette.c
+++ Projeto2/cigarette.c
@@ -2,74 +2,101 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
+#include <dispatch/dispatch.h>
 
 
 //Semaphores for ingredient production administration
-sem_t production_Administrator;
-sem_t production_Tobacco;
-sem_t production_Paper;
-sem_t production_Match;
+//sem_t production_Administrator;
+//sem_t production_Tobacco;
+//sem_t production_Paper;
+//sem_t production_Match;
+
+dispatch_semaphore_t production_Administrator;
+dispatch_semaphore_t production_Tobacco;
+dispatch_semaphore_t production_Paper;
+dispatch_semaphore_t production_Match;
 
 //Semaphores for transporters
-sem_t transporter_Tobacco;
-sem_t transporter_Paper;
-sem_t transporter_Match;
+//sem_t transporter_Tobacco;
+//sem_t transporter_Paper;
+//sem_t transporter_Match;
+
+dispatch_semaphore_t transporter_Tobacco;
+dispatch_semaphore_t transporter_Paper;
+dispatch_semaphore_t transporter_Match;
+
+//Mutex for controlling transporters access to availability variables
+pthread_mutex_t mutex;
 
 
 
-//Variables for signaling the presence of ingredients
+//Variables for signaling the presence of ingredients (availability variables)
 int isTobacco = 0;
 int isPaper = 0;
 int isMatch = 0;
 
 void* producting_Tobacco_Match(void *arg){
 	while(1){
-		sem_wait(&production_Administrator);
+		//sem_wait(&production_Administrator);
+		dispatch_semaphore_wait(production_Administrator, DISPATCH_TIME_FOREVER);
 		printf("----PRODUCTION OF TOBACCO AND MATCH IS GOING ON----\n");
 		sleep(2);
-		sem_post(&production_Tobacco);
-		sem_post(&production_Match);
+		//sem_post(&production_Tobacco);
+		dispatch_semaphore_signal(production_Tobacco);
+		//sem_post(&production_Match);
+		dispatch_semaphore_signal(production_Match);
 	}
 }
 
 void* producting_Tobacco_Paper(void *arg){
 	while(1){
-		sem_wait(&production_Administrator);
+		//sem_wait(&production_Administrator);
+		dispatch_semaphore_wait(production_Administrator, DISPATCH_TIME_FOREVER);
 		printf("----PRODUCTION OF TOBACCO AND PAPER IS GOING ON----\n");
 		sleep(2);
-		sem_post(&production_Tobacco);
-		sem_post(&production_Paper);
+		//sem_post(&production_Tobacco);
+		dispatch_semaphore_signal(production_Tobacco);
+		//sem_post(&production_Paper);
+		dispatch_semaphore_signal(production_Paper);
 	}
 }
 
 
 void* producting_Paper_Match(void *arg){
 	while(1){
-		sem_wait(&production_Administrator);
+		//sem_wait(&production_Administrator);
+		dispatch_semaphore_wait(production_Administrator, DISPATCH_TIME_FOREVER);
 		printf("----PRODUCTION OF PAPER AND MATCH IS GOING ON----\n");
 		sleep(2);
-		sem_post(&production_Paper);
-		sem_post(&production_Match);
+		//sem_post(&production_Paper);
+		dispatch_semaphore_signal(production_Paper);
+		//sem_post(&production_Match);
+		dispatch_semaphore_signal(production_Match);
 	}
 }
 
 void* smoking_With_Tobacco(void *arg){
 	while(1){
-		sem_wait(&transporter_Tobacco);
-		printf("----SMOKER WITH TOBACO IS MAKING THE CIGARETTE----\n");
+		//sem_wait(&transporter_Tobacco);
+		dispatch_semaphore_wait(transporter_Tobacco, DISPATCH_TIME_FOREVER);
+		printf("----SMOKER WITH TOBACCO IS MAKING THE CIGARETTE----\n");
 		sleep(2);
-		sem_post(&production_Administrator);
-		printf("----SMOKER WITH TOBACO IS SMOKING !!! :) ----\n");
+		//sem_post(&production_Administrator);
+		dispatch_semaphore_signal(production_Administrator);
+		printf("----SMOKER WITH TOBACCO IS SMOKING !!! :) ----\n");
 		sleep(2);
 	}
 }
 
 void* smoking_With_Match(void *arg){
 	while(1){
-		sem_wait(&transporter_Match);
+		//sem_wait(&transporter_Match);
+		dispatch_semaphore_wait(transporter_Match, DISPATCH_TIME_FOREVER);
 		printf("----SMOKER WITH MATCH IS MAKING THE CIGARETTE----\n");
 		sleep(2);
-		sem_post(&production_Administrator);
+		//sem_post(&production_Administrator);
+		dispatch_semaphore_signal(production_Administrator);
 		printf("----SMOKER WITH MATCH IS SMOKING !!! :) ----\n");
 		sleep(2);
 	}
@@ -78,12 +105,85 @@ void* smoking_With_Match(void *arg){
 
 void* smoking_With_Paper(void *arg){
 	while(1){
-		sem_wait(&transporter_Paper);
+		//sem_wait(&transporter_Paper);
+		dispatch_semaphore_wait(transporter_Paper, DISPATCH_TIME_FOREVER);
 		printf("----SMOKER WITH PAPER IS MAKING THE CIGARETTE----\n");
 		sleep(2);
-		sem_post(&production_Administrator);
+		//sem_post(&production_Administrator);
+		dispatch_semaphore_signal(production_Administrator);
 		printf("----SMOKER WITH PAPER IS SMOKING !!! :) ----\n");
 		sleep(2);
+	}
+}
+
+void* transporting_Tobacco(void *arg){
+	while(1){
+		//sem_wait(&production_Tobacco);
+		dispatch_semaphore_wait(production_Tobacco, DISPATCH_TIME_FOREVER);
+		pthread_mutex_lock(&mutex);
+		if(isPaper){
+			isPaper = 0;
+			//sem_post(&transporter_Match);
+			dispatch_semaphore_signal(transporter_Match);
+		}
+		else if(isMatch){
+			isMatch = 0;
+			//sem_post(&transporter_Paper);
+			dispatch_semaphore_signal(transporter_Paper);
+		}
+		else{
+			isTobacco = 1;
+		}
+
+		pthread_mutex_unlock(&mutex);
+	}
+}
+
+
+void* transporting_Match(void *arg){
+	while(1){
+		//sem_wait(&production_Match);
+		dispatch_semaphore_wait(production_Match, DISPATCH_TIME_FOREVER);
+		pthread_mutex_lock(&mutex);
+		if(isPaper){
+			isPaper = 0;
+			//sem_post(&transporter_Tobacco);
+			dispatch_semaphore_signal(transporter_Tobacco);
+		}
+		else if(isTobacco){
+			isTobacco = 0;
+			//sem_post(&transporter_Paper);
+			dispatch_semaphore_signal(transporter_Paper);
+		}
+		else{
+			isMatch = 1;
+		}
+
+		pthread_mutex_unlock(&mutex);
+	}
+}
+
+void* transporting_Paper(void *arg){
+	while(1){
+		//sem_wait(&production_Paper);
+		dispatch_semaphore_wait(production_Paper, DISPATCH_TIME_FOREVER);
+		pthread_mutex_lock(&mutex);
+		if(isTobacco){
+			isTobacco = 0;
+			//sem_post(&transporter_Match);
+			dispatch_semaphore_signal(transporter_Match);
+		}
+		else if(isMatch){
+			isMatch = 0;
+			//sem_post(&transporter_Tobacco);
+			dispatch_semaphore_signal(transporter_Tobacco);
+
+		}
+		else{
+			isPaper = 1;
+		}
+
+		pthread_mutex_unlock(&mutex);
 	}
 }
 
@@ -93,10 +193,22 @@ int main(){
 	pthread_t smoker_With_Tobacco, smoker_With_Match, smoker_With_Paper;
 	pthread_t transporter_Tobacco_t, transporter_Match_t, transporter_Paper_t;
 
-	sem_init(&production_Administrator, 0, 1);
-	sem_init(&production_Tobacco, 0, 0);
-	sem_init(&production_Match, 0, 0);
-	sem_init(&production_Paper, 0, 0);
+	//sem_init(&production_Administrator, 0, 1);
+	production_Administrator = dispatch_semaphore_create(1);
+	//sem_init(&production_Tobacco, 0, 0);
+	production_Tobacco = dispatch_semaphore_create(0);
+	//sem_init(&production_Match, 0, 0);
+	production_Match = dispatch_semaphore_create(0);
+	//sem_init(&production_Paper, 0, 0);
+	production_Paper = dispatch_semaphore_create(0);
+
+	transporter_Tobacco = dispatch_semaphore_create(0);
+
+	transporter_Match = dispatch_semaphore_create(0);
+
+	transporter_Paper = dispatch_semaphore_create(0);
+
+	pthread_mutex_init(&mutex, NULL);
 
 	if (pthread_create(&productor_Tobacco_Match, NULL, &producting_Tobacco_Match, NULL) != 0)
 		perror("Failed to create thread\n");
@@ -116,19 +228,63 @@ int main(){
 	if (pthread_create(&smoker_With_Paper, NULL, &smoking_With_Paper, NULL) != 0)
 		perror("Failed to create thread\n");
 
+	if (pthread_create(&transporter_Tobacco_t, NULL, &transporting_Tobacco, NULL) != 0)
+		perror("Failed to create thread\n");
+
+	if (pthread_create(&transporter_Match_t, NULL, &transporting_Match, NULL) != 0)
+		perror("Failed to create thread\n");
+
+	if (pthread_create(&transporter_Paper_t, NULL, &transporting_Paper, NULL) != 0)
+		perror("Failed to create thread\n");
+
+	if (pthread_join(productor_Tobacco_Match, NULL) != 0)
+		perror("Failed to join thread\n");
+
+	if (pthread_join(productor_Tobacco_Paper, NULL) != 0)
+		perror("Failed to join thread\n");
+
+	if (pthread_join(productor_Paper_Match, NULL) != 0)
+		perror("Failed to join thread\n");
+
+	if (pthread_join(smoker_With_Tobacco, NULL) != 0)
+		perror("Failed to join thread\n");
+
+	if (pthread_join(smoker_With_Match, NULL) != 0)
+		perror("Failed to join thread\n");
+
+	if (pthread_join(smoker_With_Paper, NULL) != 0)
+		perror("Failed to join thread\n");
+
+	if (pthread_join(transporter_Tobacco_t, NULL) != 0)
+		perror("Failed to join thread\n");
+
+	if (pthread_join(transporter_Match_t, NULL) != 0)
+		perror("Failed to join thread\n");
+
+	if (pthread_join(transporter_Paper_t, NULL) != 0)
+		perror("Failed to join thread\n");
 
 
+	pthread_mutex_destroy(&mutex);
 
+	//sem_destroy(&production_Administrator);
+	dispatch_release(production_Administrator);
 
-	sem_destroy(&production_Administrator);
-	sem_destroy(&production_Tobacco);
-	sem_destroy(&production_Paper);
-	sem_destroy(&production_Match);
+	//sem_destroy(&production_Tobacco);
+	dispatch_release(production_Tobacco);
 
+	//sem_destroy(&production_Paper);
+	dispatch_release(production_Paper);
 
+	//sem_destroy(&production_Match);
+	dispatch_release(production_Match);
+
+	dispatch_release(transporter_Tobacco);
+
+	dispatch_release(transporter_Match);
+
+	dispatch_release(transporter_Paper);
 
 
 	return 0;
 }
-
-
